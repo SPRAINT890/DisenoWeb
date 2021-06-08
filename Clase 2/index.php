8fc8cf951b5ebf6f2564c96d0af31abf02cb0980
Gaspar Morales <gasparmorales1@gmail.com>
2021-06-08 16:35:10 -0300

    styles dentro de html

---
+++ Clase 2/index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>El mejor sitio web</title>
+    <link rel="stylesheet" href="">
+    <style>
+        #capa1{
+            background-color: red;
+            width: 100%;
+            height: 150px;
+            margin-top: 0px;
+            margin-left: 0px;
+        }
+
+        .capa1{     /* clase */
+            background-color: red;
+            width: 100%;
+            height: 150px;
+            margin-top: 0px;
+            margin-left: 0px;
+        }
+    
+    
+    </style>
+</head>
+<body>
+    <div style="background-color: red; width: 100%; height: 150px; margin-top: 0px; margin-left: 0px;">
+        <p>Capa 1</p>
+    
+    </div>
+
+    <div class="capa1">
+        <p>Capa 2</p>
+    
+    </div>
+
+    <div id="capa1">
+        <p>Capa 3</p>
+    
+    </div>
+</body>
+</html>
