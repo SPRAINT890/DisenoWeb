5b9ecdb07c67f4c40e100125fcf597b6778d1224
Gaspar Morales <gasparmorales1@gmail.com>
2021-06-08 18:50:42 -0300

    dado clase 08-06

--- Clase 2/index.php
+++ Clase 2/index.php
@@ -5,41 +5,27 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>El mejor sitio web</title>
-    <link rel="stylesheet" href="">
-    <style>
-        #capa1{
-            background-color: red;
-            width: 100%;
-            height: 150px;
-            margin-top: 0px;
-            margin-left: 0px;
-        }
-
-        .capa1{     /* clase */
-            background-color: red;
-            width: 100%;
-            height: 150px;
-            margin-top: 0px;
-            margin-left: 0px;
-        }
-    
-    
-    </style>
+    <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
-    <div style="background-color: red; width: 100%; height: 150px; margin-top: 0px; margin-left: 0px;">
+    <div id="capa1" class="capa textoCapa">
         <p>Capa 1</p>
     
     </div>
 
-    <div class="capa1">
+    <div id="capa2" class="capa textoCapa">
         <p>Capa 2</p>
     
     </div>
 
-    <div id="capa1">
+    <div id="capa3" class="capa textoCapa">
         <p>Capa 3</p>
     
+    </div>
+
+    <div id="capa4" class="capa">
+        <p>Capa 4</p>
+
     </div>
 </body>
 </html>
